2ff75a4923e02e5001e46f16b9177365cc0a2804
louisinayinde <lousinayinde@gmail.com>
2022-07-06 22:13:29 +0200

    beginning footer

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
 import './Newsletter.css';
+import { subHeading } from '../../components'
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className='app__newsletter'>
+    <div className='app__newsletter-heading'>
+      <subHeading title="newsletter" />
+      <h1 className='headtext__cormorant'>Subscribe To Our Newsletter</h1>
+      <p className='p__opensans'>And Never Miss Latest Updates!</p>
+    </div>
   </div>
 );
 
